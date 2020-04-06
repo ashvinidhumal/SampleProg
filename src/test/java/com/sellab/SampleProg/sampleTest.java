bd06936b50f7c6696b90edd64dc114dc9d50098d
ashvinidhumal <ashvinidhumal39@gmail.com>
2020-04-06 18:03:46 +0530

    initial commit

---
+++ src/test/java/com/sellab/SampleProg/sampleTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.sellab.SampleProg;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author ashvi
+ *
+ */
+public class sampleTest {
+
+	@Test
+	public void name()
+	{
+		System.out.println("ashvini");
+	}
+}
